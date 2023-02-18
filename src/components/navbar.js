b3f376a492707154b8e731f91b5bcecf2d1e3510
Xyphes <willy.somkhit@epita.fr>
2023-02-18 20:14:55 +0100

    test

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 const navbar =()=>{
     return (
         <div>
-            <Link to="/">Sleepless</Link>
-            <Link to="/Monumap">Monumap</Link>
+            <Link to="/">Sleepless </Link>
+            <Link to="/Monumap">Monumap </Link>
             <Link to="/Coven">Coven</Link>
         </div>
     )
